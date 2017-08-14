f22a1154215a4ac351dd530aa01caa94540bd27c
soccerdroid <mguarand@espol.edu.ec>
2017-08-14 10:34:12 -0500

    trabajando en vista de get oil price

--- practica_soap/views.py
+++ practica_soap/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from zeep import Client
+from .forms import *
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import *
 # Create your views here.
+
+
+def getOilPrice(request):
+	if(request.method=="POST"):
+		form=getOilPriceForm(request.POST)
+		if(form.is_valid()):
+			lenguaje=form.get_cleaned_data["lenguaje"]
+			dia=form.get_cleaned_data["dia"]
+			mes=form.get_cleaned_data["mes"]
+			anio=form.get_cleaned_data["anio"]
+			client = Client('http://www.pttplc.com/webservice/pttinfo.asmx?WSDL')
+			result = client.service.GetOilPrice(lenguaje, dia, mes,anio)
+			return render(request,'practica_soap/results_getOilPrice.html',{'results':results})
+
+		else:
+			print('formulario no valido')
+	else:
+		form=getOilPriceForm()
+	return render(request,'practica_soap/formulario_getOilPrice.html',{'form':form})
+

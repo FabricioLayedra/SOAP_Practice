84d6e24f9eb5dcbd567a57dedac154ee24585bb9
soccerdroid <mguarand@espol.edu.ec>
2017-08-14 10:04:39 -0500

    app creada

--- daw/settings.py
+++ daw/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'practica_soap',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ practica_soap/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PracticaSoapConfig(AppConfig):
+    name = 'practica_soap'
